9b0a3772a2cbe1b12575b1a68fb534f87cfe2db1
juantoniolee888 <jlee89@nd.edu>
2023-01-06 13:43:36 -0600

    finished base template

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, url_for
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def main():
+    return render_template('base.html')
+
+
+@app.route('/home')
+def home():
+	return render_template('home.html')
