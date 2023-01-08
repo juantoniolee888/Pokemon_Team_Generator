e98b3085ae3872d4c14258a4f5b2e7b51d3fca28
juantoniolee888 <jlee89@nd.edu>
2023-01-08 17:26:13 -0600

    initialize forms and fix links

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__, static_url_path='/static')
+
 
 @app.route('/')
 def main():
-    return render_template('base.html')
+    return render_template('login.html')
 
-
-@app.route('/home')
+@app.route('/home', methods = ['POST', 'GET'])
 def home():
-	return render_template('home.html')
+	if request.method == 'POST':
+		form_data = request.form
+		username = form_data.get("username")
+		password = form_data.get("password")
+		return render_template('home.html',username=username)
+
+@app.route('/<username>/home')
+def user_home(username):
+		return render_template('home.html',username=username)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
